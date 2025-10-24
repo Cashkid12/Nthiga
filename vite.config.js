27f321b64d224c4e87defec32113f9df6da7b940
joe nthiga <joenthiga678@gmail.com>
2025-10-24 13:26:43 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: []
+    }
+  },
+  // Add this for SPA routing:
+  server: {
+    historyApiFallback: true
+  }
 })
